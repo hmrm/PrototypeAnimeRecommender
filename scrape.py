35785fc3326651a88e02d246e5851b5e3b7a06a5
hmrm <hmrmaxwell@gmail.com>
2012-08-13 23:47:00 -0700

    the basic scraper is done

--- scrape.py
+++ scrape.py
@@ -1,3 +1,10 @@
 from bs4 import BeautifulSoup
+
+html_doc = open("html_doc", "r").read()
+
 soup = BeautifulSoup(html_doc)
-print soup.prettify()
+
+for text in soup.find_all('table'):
+    print text
+    for link in text.find_all('a'):
+        print link.get_text()
