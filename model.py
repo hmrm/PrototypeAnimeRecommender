10a8945c0c5c55857129eaa4e5e80e3d6b2748f7
hmrm <hmrmaxwell@gmail.com>
2012-08-16 12:35:52 -0700

    it works, now I'm lost

--- model.py
+++ model.py
@@ -1,24 +1,24 @@
 import numpy as np
 import sys
-matrix = np.matrix( map(lambda x: map(lambda y: float(y), x.split()), sys.stdin.readlines()) )
+import pca
+A = np.matrix(map(lambda x: map(lambda y: float(y), x.split()), sys.stdin.readlines()))
 print "Original matrix:"
-print matrix
-U, s, V = np.linalg.svd( matrix )
+print A
+
+pca.Center(A)
+
+print "Centered and Scaled matrix:"
+print A
+p = pca.PCA(A, fraction=0.80)
 print "U:"
-print U
-print "sigma:"
-print s
-print "VT:"
-print V
-dimensions = 1
-rows,cols = np.matrix.shape
-#Dimension reduction, build SIGMA'
-for index in xrange(dimensions, rows):
- s[index]=0
-print "reduced sigma:"
-print s
-#Reconstruct MATRIX'
-reconstructedMatrix= np.dot(np.dot(U,np.linalg.diagsvd(s,len(matrix),len(V))),V)
-#Print transform
-print "reconstructed:"
-print reconstructedMatrix
+print p.U
+print "d:"
+print p.d
+print "Vt:"
+print p.Vt
+print "eigenvalues:"
+print p.eigen
+print "number of pcs:"
+print p.npc
+print "udvt:"
+print 
