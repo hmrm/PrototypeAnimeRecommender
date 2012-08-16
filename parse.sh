52481aae24b2d5d471e902a33f0ab8c5a69c8738
hmrm <hmrmaxwell@gmail.com>
2012-08-16 11:54:18 -0700

    finished running the scraper

--- parse.sh
+++ parse.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+i=1
+
 while read -r line
 do
-    echo $line >&2
+    echo $i $line >&2
+    i=$(( $i + 1))
     curl "http://mal-api.com/animelist/"$line | ruby parse_json.rb
 done
--- parse_json.rb
+++ parse_json.rb
@@ -7,7 +7,7 @@ ratings = Array.new(15177)
 ratings.map! { |x| 0 }
 
 data.each do |dict|
-  ratings[dict["id"].to_i] = dict["score"].to_f.to_i
+  ratings[dict["id"].to_i] = dict["score"].to_f.to_i unless dict["id"].to_i >= 15177
 end
 
 print ratings
