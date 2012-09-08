4ce31a60e77290526674be74d5ef05ceb9dd41c0
hmrm <hmrmaxwell@gmail.com>
2012-09-07 17:03:46 -0700

    cross validation code

---
+++ geterrors_knn.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm $1"_errors"
+echo "Getting Errors for "$1" Eigenvalues"
+for i in {0..9}
+do
+    echo $i"-th Subsample"
+    python model_knn.py $1 $i"_tvalidation_re" $i"_vvalidation_re" 75 mean < $i"_training_re" >> $j"_errors"
+done
